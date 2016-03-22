ba9ae1030d3e794f1a2c7ecb73c125d152892067
davask <davask.42@gmail.com>
2016-03-22 12:47:31 +0100

    Update process

--- dwl-init-2-proxy-reverse.sh
+++ dwl-init-2-proxy-reverse.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "PROCESSING ENV VAR TO /tmp/dwl_proxy_projects.yml";
-/tmp/dwl-env2yaml-0-proxy.sh
-echo "PROCESSING /tmp/dwl_proxy_projects.yml TO VIRTULHOSTS";
-perl /tmp/dwl-yaml2virtualhost-0-proxy.pl
+echo "Proxy reverse initialized";
